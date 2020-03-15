4c10c6deade66181051608ba0493e34239119456
Naoki Hirayama <hn-0905da@yahoo.ne.jp>
2020-03-15 04:49:31 +0000

    Finish add-basic-info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,10 +45,16 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
   
-   def edit_basic_info
+  def edit_basic_info
   end
 
   def update_basic_info
+    if @user.update_attributes(basic_info_params)
+      flash[:success] = "#{@user.name}の基本情報を更新しました。"
+    else
+      flash[:danger] = "#{@user.name}の更新は失敗しました。<br>" + @user.errors.full_messages.join("<br>")
+    end
+    redirect_to user_url
   end
   
   
@@ -58,7 +64,12 @@ class UsersController < ApplicationController
   def user_params
       params.require(:user).permit(:name,:email,:department,:password,:password_confirmation)
   end 
-    
+  
+  def basic_info_params
+      params.require(:user).permit(:department, :basic_time, :work_time)
+  end 
+  
+  
   def set_user
       @user = User.find(params[:id])
   end
