b7300311d7f381e32244ba267b894f7c2611fa0f
Naoki Hirayama <hn-0905da@yahoo.ne.jp>
2020-03-17 14:26:30 +0000

    11.2から

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -71,26 +71,9 @@ class UsersController < ApplicationController
   end 
   
   
-  def set_user
-      @user = User.find(params[:id])
-  end
   
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "ログインしてください。"
-      redirect_to login_url
-    end  
-  end
   
-  def correct_user
-    redirect_to(root_url) unless current_user?(@user)
-  end  
   
-  def admin_user
-      redirect_to root_url unless current_user.admin?
-  end
-
 end
 
 
